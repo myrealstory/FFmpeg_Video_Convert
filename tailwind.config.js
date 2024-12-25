40c29960e77325d2b0d242921e103558fa5c9caf
Gordon Lew <104052973+myrealstory@users.noreply.github.com>
2024-12-25 19:48:09 +0800

    update

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,tsx,jsx}"],
+  theme: {
+    theme: {
+      extend: {
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+          gray: "#e4e3e3",
+          primaryDark: "#1a202c",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
