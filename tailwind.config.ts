5149147b7aa120a82edfa4f42840a60f280d5afd
Gordon Lew <104052973+myrealstory@users.noreply.github.com>
2024-12-25 18:06:10 +0800

    測試

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 包括動態路由的所有文件
-    "./pages/**/*.{js,ts,jsx,tsx}",  // 傳統 Pages Router 的文件
-    "./components/**/*.{js,ts,jsx,tsx}", // 組件文件
-    "./styles/**/*.{css,scss}",      // 自定義樣式文件
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,scss}",
   ],
   theme: {
     extend: {
